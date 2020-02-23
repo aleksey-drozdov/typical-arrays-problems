e69c2b522a0bf58c46ce61e5793bcf0e41789d49
Lesha Drozdov <leshadrozdov@gmail.com>
2020-02-23 14:23:51 +0300

    feat: add search min, max and avg numbers on array

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!array || array.length == 0) {
+    return 0;
+  } else {
+    var minNum = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] < minNum) {
+        minNum = array[i]
+      }
+    }
+    return minNum
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!array || array.length == 0) {
+    return 0;
+  } else {
+    var maxNum = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] > maxNum) {
+        maxNum = array[i]
+      }
+    }
+    return maxNum
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!array || array.length == 0) {
+    return 0;
+  } else {
+    var avgNum = 0;
+    for (let i = 0; i < array.length; i++) {
+      avgNum += array[i];
+    }
+    avgNum = avgNum / array.length;
+    return avgNum;
+  }
 }
